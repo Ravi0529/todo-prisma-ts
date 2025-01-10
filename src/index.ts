2b0016b57244d38ffe0b8e3268b61d79bbc8c654
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-01-11 01:30:49 +0530

    added basic todo functionalities

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client"; // importing prisma client
 
 const prisma = new PrismaClient(); // connection setup
 
+// create user
 const createUser = async (username: string, password: string, firstName: string, lastName: string, email: string) => {
     const res = await prisma.user.create({
         data: {
@@ -14,4 +15,55 @@ const createUser = async (username: string, password: string, firstName: string,
     })
     console.log(res);
 }
-// createUser("");
+
+// create todo
+const createTodo = async (userId: number, title: string, description: string) => {
+    const todo = await prisma.todo.create({
+        data: {
+            userId,
+            title,
+            description
+        }
+    })
+    console.log(todo);
+}
+
+// get all todos
+const getTodos = async (userId: number) => {
+    const todos = await prisma.todo.findMany({
+        where: {
+            userId
+        }
+    })
+    console.log(todos);
+}
+
+// get todos and user details
+const getTodosAndUserDetails = async (userId: number) => {
+    const todos = await prisma.todo.findMany({
+        where: {
+            userId
+        },
+        select: {
+            user: true,
+            title: true,
+            description: true
+        }
+    })
+    console.log(todos);
+}
+
+// update todo
+const updateTodo = async (todoId: number, title: string, description: string, done: boolean) => {
+    const updatedTodo = await prisma.todo.update({
+        where: {
+            id: todoId
+        },
+        data: {
+            title,
+            description,
+            done
+        }
+    })
+    console.log(updatedTodo);
+}
