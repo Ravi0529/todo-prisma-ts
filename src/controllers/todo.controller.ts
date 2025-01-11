88f4659d4d16e3509f3d5f378b3137f233b3ab07
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-01-11 16:27:47 +0530

    todo added

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,15 +1,45 @@
-export const createTodo = async () => {
-    
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { title, description, userId } = req.body;
+
+        if (!title || !description || !userId) {
+            res.status(400).json({ message: "Title, description, and userId are required" });
+        }
+
+        const newTodo = await prisma.todo.create({
+            data: {
+                title,
+                description,
+                userId
+            },
+        });
+
+        res.status(201).json({ newTodo });
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error });
+    }
 }
 
-export const updateTodo = async () => {
-    
-}
+export const getAllTodos = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { userId } = req.params;
+        const NumericId = parseInt(userId);
 
-export const getAllTodos = async () => {
-    
-}
+        if (isNaN(NumericId)) {
+            res.status(400).json({ message: "Invalid userId" });
+        }
 
-export const todosAndUser = async () => {
-    
+        const todos = await prisma.todo.findMany({
+            where: { userId: NumericId }
+        });
+
+        res.status(200).json({ todos })
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error });
+    }
 }
--- src/routes/todo.route.ts
+++ src/routes/todo.route.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import {createTodo, updateTodo, getAllTodos, todosAndUser} from "../controllers/todo.controller.js";
+import { createTodo, getAllTodos } from "../controllers/todo.controller.js";
 
 const router = express.Router();
 
 router.post("/createTodo", createTodo);
-router.post("/updateTodo", updateTodo);
-router.get("/allTodos", getAllTodos);
-router.get("/displayAll", todosAndUser);
+router.get("/allTodos/:id", getAllTodos);
 
 export default router;
